45707e98bfbfd6e71c0ee4cfec8bc74f9a2b21f2
wangruofeng <261039521@qq.com>
2016-04-14 23:47:18 +0800

    init Associated Object Demo
    init Associated Object Demo

--- AssociatedObjectDemo/ViewController.m
+++ AssociatedObjectDemo/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "UIImagePickerController+RFBlocks.h"
+#import "UIButton+RFBlcoks.h"
+#import "UIViewController+RFPresentPopover.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *button;
 
 @end
 
@@ -17,11 +21,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.button.rf_buttonClickBlock = ^(UIButton *button){
+        NSLog(@"%@ clicked",button);
+    };
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showImagePickerController:(id)sender {
+    
+    //    [self test1];
+    [self test2];
+}
+
+
+- (IBAction)showPoperViewController:(id)sender {
+    
+    UIViewController *vc = [UIViewController new];
+    vc.view.backgroundColor = [UIColor orangeColor];
+    
+    [self rf_presentPopoverWithViewController:vc fromRect:self.button.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionRight animated:YES];
+}
+
+- (void)test1
+{
+    __weak typeof(self)weakSelf = self;
+    
+    UIImagePickerController *picker =  [UIImagePickerController rf_imagePickerWithFinishBlock:^(NSDictionary *info) {
+        NSLog(@"finish picke image\n info:%@",info);
+        
+        UIImage *img = info[@"UIImagePickerControllerOriginalImage"];
+        weakSelf.view.layer.contents = (id)img.CGImage;
+    }];
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (void)test2
+{
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    
+    __weak typeof(self)weakSelf = self;
+    
+    picker.rf_finishBlock = ^(NSDictionary *info) {
+        NSLog(@"finish picke image\n info:%@",info);
+        
+        UIImage *img = info[@"UIImagePickerControllerOriginalImage"];
+        weakSelf.view.layer.contentsGravity = kCAGravityResizeAspect;
+        weakSelf.view.layer.contents = (id)img.CGImage;
+    };
+    
+    [self presentViewController:picker animated:YES completion:nil];
 }
 
 @end
